cfe1c277c15cc21e531cf81c4f0a7c54d604a1e1
Joao3run <57202776+Joao3run@users.noreply.github.com>
2021-04-03 01:04:41 -0300

    Update task.dart

--- lib/app/shared/models/task.dart
+++ lib/app/shared/models/task.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Task {
-  int? id;
+  String? id;
   String? title;
   String? description;
   bool done;
@@ -10,4 +14,26 @@ class Task {
     this.description,
     this.done = false,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'description': description,
+      'done': done,
+    };
+  }
+
+  factory Task.fromMap(Map<String, dynamic> map, String documentId) {
+    return Task(
+      id: documentId,
+      title: map['title'],
+      description: map['description'],
+      done: map['done'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Task.fromJson(String source, String documentId) =>
+      Task.fromMap(json.decode(source), documentId);
 }
